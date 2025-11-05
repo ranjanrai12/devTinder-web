5e7ad884b1a4691939b2b4c4265879612db0a36f
ranjanrai <ranjankumarrai1992@gmail.com>
2025-11-05 12:14:03 +0530

    env config updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,20 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      // remote setup
+      // "/api": {
+      //   target: "http://63.177.97.113",
+      // },
+
+      // local setup
+      '/api': {
+        target:'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    },
+  },
   plugins: [react(), tailwindcss()],
 });
