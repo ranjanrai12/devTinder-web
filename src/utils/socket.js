aa2b59fc9e838e22ab18ebe1e256edca6baf7fc0
ranjanrai <ranjankumarrai1992@gmail.com>
2025-11-06 18:02:07 +0530

    approach is handled in env file

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,10 +1,13 @@
 import io from "socket.io-client";
 
 export const createSocketConnection = () => {
-  if (location.hostname === "localhost") {
-    return io(import.meta.env.VITE_SOCKET_URL);
-  }
-  return io("/", {
-    path: "/api/socket.io",
+  //   if (location.hostname === "localhost") {
+  //     return io(import.meta.env.VITE_SOCKET_URL);
+  //   }
+  //   return io("/", {
+  //     path: "/api/socket.io",
+  //   });
+  return io(import.meta.env.VITE_SOCKET_URL, {
+    withCredentials: true,
   });
 };
