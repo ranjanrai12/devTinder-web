99d54c00beb56dde6b7df7ac3dbf9ca128f3568e
ranjanrai <ranjankumarrai1992@gmail.com>
2025-11-06 18:04:08 +0530

    commented code removed

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,12 +1,6 @@
 import io from "socket.io-client";
 
 export const createSocketConnection = () => {
-  //   if (location.hostname === "localhost") {
-  //     return io(import.meta.env.VITE_SOCKET_URL);
-  //   }
-  //   return io("/", {
-  //     path: "/api/socket.io",
-  //   });
   return io(import.meta.env.VITE_SOCKET_URL, {
     path: import.meta.env.VITE_SOCKET_PATH,
     withCredentials: true,
