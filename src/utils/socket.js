4f9c0f61e040d9835ded6ae79a3e057992b71c03
ranjanrai <ranjankumarrai1992@gmail.com>
2025-11-06 17:24:17 +0530

    base url updated

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,10 @@
 import io from "socket.io-client";
 
 export const createSocketConnection = () => {
+  if (location.hostname === "localhost") {
     return io(import.meta.env.VITE_SOCKET_URL);
-}
+  }
+  return io("/", {
+    path: "/api/socket.io",
+  });
+};
